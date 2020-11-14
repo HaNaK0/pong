a788faed03a783d0577564b043779d94a88bddd3
HaNaK0 <saturn-v@hotmail.com>
2020-11-14 22:01:57 +0100

    Pong tutorial part 3: Moving the paddles

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use amethyst::{
 };
 
 mod pong;
+mod systems;
 
 use crate::pong::Pong;
 
@@ -20,18 +21,25 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
+	let display_config_path = app_root.join("config").join("display.ron");
+	
+	let binding_path = app_root.join("config").join("bindings.ron");
 
-    let game_data = GameDataBuilder::default()
-    .with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-        .with_plugin(
-            RenderToWindow::from_config_path(display_config_path)?
-                .with_clear([0.0, 0.0, 0.0, 1.0])
-        )
-        .with_plugin(RenderFlat2D::default())
-	)?
-	.with_bundle(TransformBundle::new())?;
+	let input_bundle = InputBundle::<StringBindings>::new()
+		.with_bindings_from_file(binding_path)?;
+
+	let game_data = GameDataBuilder::default()
+		.with_bundle(TransformBundle::new())?
+		.with_bundle(input_bundle)?
+		.with(systems::PaddleSystem, "paddle_system", &["input_system"])
+		.with_bundle(
+			RenderingBundle::<DefaultBackend>::new()
+			.with_plugin(
+				RenderToWindow::from_config_path(display_config_path)?
+					.with_clear([0.0, 0.0, 0.0, 1.0])
+			)
+			.with_plugin(RenderFlat2D::default())
+		)?;
 
     let asset_dir = app_root.join("assets");
     let mut game = Application::new(asset_dir, Pong, game_data)?;
